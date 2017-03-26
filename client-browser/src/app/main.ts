92ab4970f246b6f9bc1589cc133db03e67dd512d
Lucas Espindola <espdlucas@gmail.com>
2017-03-26 16:39:31 -0300

    adjust columns in pacient

--- client-browser/src/app/main.ts
+++ client-browser/src/app/main.ts
@@ -4,4 +4,17 @@ import {Component} from '@angular/core';
   selector: 'fountain-app',
   template: require('./main.html')
 })
-export class MainComponent {}
+export class MainComponent {
+  convenios = [
+    {name: 'Particular', value: 'particular'},
+    {name: 'Unimed', value: 'unimed'},
+    {name: 'Amil', value: 'amil'},
+    {name: 'Golden', value: 'golden'},
+    {name: 'Bradesco', value: 'bradesco'},
+    {name: 'Ipalerj', value: 'ipalerj'},
+    {name: 'Dix', value: 'dix'},
+    {name: 'Miller', value: 'miller'},
+    {name: 'Marítima', value: 'maritma'},
+  ];
+
+}
