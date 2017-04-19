1bb968dbdf0091bff273acc3973c1d9ec98fb0c5
Lucas Espindola <espdlucas@gmail.com>
2017-04-18 23:57:25 -0300

    fix(server) update conn strings

--- client-browser2/src/pages/login/login.ts
+++ client-browser2/src/pages/login/login.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {AlertController, LoadingController, NavController} from 'ionic-angular';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {AlertController, LoadingController, NavController, MenuController} from 'ionic-angular';
 import {LoginService} from '../../providers/login.service';
 import {SearchPage} from '../../pages/search/search';
 import {RegisterPage} from '../../pages/register/register';
@@ -7,7 +7,7 @@ import {RegisterPage} from '../../pages/register/register';
 @Component({
   templateUrl: 'login.html'
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, OnDestroy  {
   isLoading: boolean;
   username: string;
   password: string;
@@ -15,13 +15,16 @@ export class LoginPage implements OnInit {
   constructor(private alertCtrl: AlertController,
               private loadingCtrl: LoadingController,
               private navCtrl: NavController,
-              private loginService: LoginService) {
-
-
+              private loginService: LoginService,
+              private menuCtrl: MenuController) {
   }
 
   ngOnInit() {
-      
+      this.menuCtrl.enable(false, 'appMenu');
+  }
+
+  ngOnDestroy() {
+      this.menuCtrl.enable(true, 'appMenu');
   }
 
   login(){
--- client-browser2/src/providers/constants.ts
+++ client-browser2/src/providers/constants.ts
@@ -2,8 +2,10 @@
  * Created by espdlucas on 3/30/17.
  */
 
-export const SERVER_URL = 'http://localhost:5000';
-export const API_URL = 'http://localhost:5000/api';
+//export const SERVER_URL = 'http://localhost:5000';
+//export const API_URL = 'http://localhost:5000/api';
+export const SERVER_URL = 'https://mednuvem.azurewebsites.net';
+export const API_URL = 'https://mednuvem.azurewebsites.net/api';
 
 export const CLIENT_OAUTH2_CONFIG = {
     clientId: 'mednuvemApp',
